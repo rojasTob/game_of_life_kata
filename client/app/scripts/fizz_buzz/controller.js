be60f64cc7ebefe8a5eb50722596867bb6094396
Angie Rojas <nataliart4@gmail.com>
2016-08-11 15:05:05 -0500

    Added tests and functions in order to analyze the list of numbers.

--- client/app/scripts/fizz_buzz/controller.js
+++ client/app/scripts/fizz_buzz/controller.js
@@ -4,6 +4,38 @@ angular.module('Fizz_Buzz')
     .controller('fizz_buzz', function ($scope) {
 
         $scope.controller_loaded = 'Fizz Buzz loaded';
+        $scope.top_number = 0;
+        $scope.resultsGame = [];
+
+        $scope.generate_numbers = function(){
+            for(var position = 0 ; position < $scope.top_number ; position++){
+                var number = position + 1;
+                $scope.resultsGame[position] = $scope.analyzeResult(number);
+            }
+        };
+
+        $scope.divisible_by_three = function(number){
+            return ((number % 3) === 0);
+        };
+
+        $scope.divisible_by_five = function(number){
+            return ((number % 5) === 0);
+        };
+
+        $scope.analyzeResult = function(number){
+            var result = '';
+            if($scope.divisible_by_three(number)){
+                result = result.concat('Fizz');
+            }
+
+            if($scope.divisible_by_five(number)){
+                result = result.concat('Buzz');
+            }
+
+            return result ? result : number;
+        };
+
+
 
     })
     .config(function ($routeProvider) {
--- client/app/scripts/fizz_buzz/specs/controller_spec.js
+++ client/app/scripts/fizz_buzz/specs/controller_spec.js
@@ -16,6 +16,62 @@ describe('Controller: fizz_buzz', function () {
         it('should set "controller_loaded" variable in scope', function () {
             expect(scope.controller_loaded).toContain('loaded');
         });
+
+        it('should have a top number initialized', function(){
+            expect(scope.top_number).toEqual(0);
+        });
+
+        it('should have a result array initialized', function(){
+            expect(scope.resultsGame).toEqual([]);
+        });
+
+        it('should generate numbers from 1 to 10', function(){
+            scope.top_number = 10;
+            scope.generate_numbers();
+            expect(scope.resultsGame.length).toEqual(10);
+        });
+
+        it('if a number is divisible by 3 should return true', function(){
+            expect(scope.divisible_by_three(3)).toEqual(true);
+        });
+
+        it('if a number is not divisible by 3 should return false', function(){
+            expect(scope.divisible_by_three(4)).toEqual(false);
+        });
+
+        it('if a number is divisible by 5 should return true', function(){
+            expect(scope.divisible_by_five(5)).toEqual(true);
+        });
+
+        it('if a number is not divisible by 5 should return false', function(){
+            expect(scope.divisible_by_five(4)).toEqual(false);
+        });
+
+        it('when a number is divisible by 3, the result should have the word Fizz instead of the number', function(){
+            expect(scope.analyzeResult(6)).toEqual('Fizz');
+        });
+
+        it('when a number is divisible by 5, the result should have the word Buzz instead of the number', function(){
+            expect(scope.analyzeResult(10)).toEqual('Buzz');
+        });
+
+        it('when a number is divisible by 3 and 5, the result should have the word FizzBuzz instead of the number', function(){
+            expect(scope.analyzeResult(15)).toEqual('FizzBuzz');
+        });
+
+        it('when a number is not divisible by 3 or 5, the result should have the same number', function(){
+            expect(scope.analyzeResult(4)).toEqual(4);
+        });
+
+        it('should generate numbers from 1 to 10', function(){
+            scope.top_number = 16;
+            scope.generate_numbers();
+            expect(scope.resultsGame[2]).toEqual('Fizz');
+            expect(scope.resultsGame[3]).toEqual(4);
+            expect(scope.resultsGame[4]).toEqual('Buzz');
+            expect(scope.resultsGame[14]).toEqual('FizzBuzz');
+        });
+
     });
 
     describe('when going to /fizz_buzz', function () {
